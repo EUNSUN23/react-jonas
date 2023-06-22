b6aaf2ba279540a8023142d2a48c1b050f270bfd
eskim <eskim@acrofuture.com>
2023-06-22 13:34:02 +0900

    StarRating컴포넌트> 재사용성높이기 - style관련 props, state전달용 props추가

--- untitled/src/index.js
+++ untitled/src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from "./StarRating";
 
+// StarRating의 rating이 컴포넌트 바깥에서도 쓰여야 하는경우.
+function Test() {
+    const [movieRating, setMovieRating] = useState(0);
+
+
+    return (
+        <div>
+            <StarRating maxRating={10} color='blue' onSetRating={setMovieRating}/>
+            <div>This movie was rated {movieRating} stars</div>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-      <StarRating maxRating={5}/>
+      <StarRating maxRating={5} defaultRating={3} message={['Terrible','Bad','Okay','Good','Amazing']}/>
+      <Test/>
   </React.StrictMode>
 );
 
