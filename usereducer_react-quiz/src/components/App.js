cdbe55dfd4b6913628cd02bfdb77db554c5eef66
eskim <eskim@acrofuture.com>
2023-06-30 15:46:46 +0900

    react-quiz 완성

--- usereducer_react-quiz/src/components/App.js
+++ usereducer_react-quiz/src/components/App.js
@@ -7,6 +7,11 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Timer from "./Timer";
+import Footer from "./Footer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
     questions: [],
@@ -14,7 +19,9 @@ const initialState = {
     status: "loading",
     index: 0,
     answer: null,
-    points: 0
+    points: 0,
+    highscore: 0,
+    secondsRemaining: null
 }
 
 function reducer(state, action) {
@@ -24,7 +31,7 @@ function reducer(state, action) {
         case 'dataFailed':
             return {...state, status: 'error'};
         case 'start':
-            return {...state, status: 'active'};
+            return {...state, status: 'active', secondsRemaining: state.questions.length * SECS_PER_QUESTION};
         case 'newAnswer':
             const question = state.questions.at(state.index);
             return {
@@ -35,14 +42,36 @@ function reducer(state, action) {
                     : state.points
             };
         case 'nextQuestion':
-            return {...state, index: state.index + 1, answer: null}
+            return {...state, index: state.index + 1, answer: null};
+        case 'finish':
+            return {
+                ...state,
+                status: 'finished',
+                highscore: state.points > state.highscore ? state.points : state.highscore
+            };
+        case 'restart':
+            return {...initialState, questions: state.questions, status: 'ready'};
+        case 'tick':
+            return {
+                ...state,
+                secondsRemaining: state.secondsRemaining - 1,
+                status: state.secondsRemaining === 0 ? 'finished' : state.status
+            };
         default:
             throw new Error("Action is unknown");
     }
 }
 
 export default function App() {
-    const [{questions, status, index, answer, points}, dispatch] = useReducer(reducer, initialState);
+    const [{
+        questions,
+        status,
+        index,
+        answer,
+        points,
+        highscore,
+        secondsRemaining
+    }, dispatch] = useReducer(reducer, initialState);
 
     const numQuestions = questions.length;
     const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0);
@@ -63,11 +92,19 @@ export default function App() {
                 {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
                 {status === "active" && (
                     <>
-                        <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer}/>
+                        <Progress index={index} numQuestions={numQuestions} points={points}
+                                  maxPossiblePoints={maxPossiblePoints} answer={answer}/>
                         <Question question={questions[index]} dispatch={dispatch} answer={answer}/>
-                        <NextButton dispatch={dispatch} answer={answer}/>
+                        <Footer>
+                            <Timer dispatch={dispatch} secondsRemaining={secondsRemaining}/>
+                            <NextButton dispatch={dispatch} answer={answer} numQuestions={numQuestions} index={index}/>
+                        </Footer>
+
                     </>
                 )}
+                {status === 'finished' &&
+                    <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highscore={highscore}
+                                  dispatch={dispatch}/>}
             </Main>
         </div>
     )
