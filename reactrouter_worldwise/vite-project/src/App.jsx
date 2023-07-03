e99c93acbadaff848f81a7a81fe4a7e478758703
eskim <eskim@acrofuture.com>
2023-07-03 13:29:47 +0900

    [reactrouter_worldwise] index route 추가

--- reactrouter_worldwise/vite-project/src/App.jsx
+++ reactrouter_worldwise/vite-project/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
                 <Route path="pricing" element={<Pricing/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="app" element={<AppLayout/>}> {/* nested routes */}
+                    <Route index element={<p>LIST</p>}/> {/* root path (/app)에 대한 매칭 */}
                     <Route path='cities' element={<p>List of cities</p>}/>
                     <Route path='countries' element={<p>Countries</p>}/>
                     <Route path='form' element={<p>Form</p>}/>
