40cdffce868268abffb452486d7a222103b5d58a
eskim <eskim@acrofuture.com>
2023-07-03 21:02:33 +0900

    [reactrouter_worldwise] context API사용(CitiesContext추가)

--- reactrouter_worldwise/vite-project/src/App.jsx
+++ reactrouter_worldwise/vite-project/src/App.jsx
@@ -7,53 +7,33 @@ import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList.jsx";
-import {useEffect, useState} from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City.jsx";
 import Form from "./components/Form.jsx";
+import {CitiesProvider} from "./context/CitiesContext";
 
-const BASE_URL = 'http://localhost:8000';
 
 function App() {
-    const [cities, setCities] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        async function fetchCities() {
-            try {
-                setIsLoading(true);
-                const res = await fetch(`${BASE_URL}/cities`);
-                const data = await res.json();
-                setCities(data);
-            } catch (e) {
-                alert("There was an error loading data...");
-            } finally {
-                setIsLoading(false);
-            }
-        }
-
-        fetchCities();
-    }, []);
-
-
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="product" element={<Product/>}/>
-                <Route path="pricing" element={<Pricing/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="app" element={<AppLayout/>}> {/* nested routes */}
-                    <Route index
-                           element={<Navigate to='cities' replace/>}/> {/* root path (/app)에 대한 매칭 */} {/* Navigate : cities로 리다이렉팅 replace : history 대체 */}
-                    <Route path='cities' element={<CityList cities={cities} isLoading={isLoading}/>}/>
-                    <Route path='cities/:id' element={<City/>}/> {/* :id -> parameter와 매칭되는 key가 된다. */}
-                    <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading}/>}/>
-                    <Route path='form' element={<Form/>}/>
-                </Route>
-                <Route path="*" element={<PageNotFound/>}/> {/* 위 path에 모두 해당하지 않는 url에 매칭 */}
-            </Routes>
-        </BrowserRouter>
+        <CitiesProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="product" element={<Product/>}/>
+                    <Route path="pricing" element={<Pricing/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="app" element={<AppLayout/>}> {/* nested routes */}
+                        <Route index
+                               element={<Navigate to='cities' replace/>}/> {/* root path (/app)에 대한 매칭 */} {/* Navigate : cities로 리다이렉팅 replace : history 대체 */}
+                        <Route path='cities' element={<CityList />}/>
+                        <Route path='cities/:id' element={<City/>}/> {/* :id -> parameter와 매칭되는 key가 된다. */}
+                        <Route path='countries' element={<CountryList />}/>
+                        <Route path='form' element={<Form/>}/>
+                    </Route>
+                    <Route path="*" element={<PageNotFound/>}/> {/* 위 path에 모두 해당하지 않는 url에 매칭 */}
+                </Routes>
+            </BrowserRouter>
+        </CitiesProvider>
     );
 }
 
--- reactrouter_worldwise/vite-project/src/components/CityList.jsx
+++ reactrouter_worldwise/vite-project/src/components/CityList.jsx
@@ -2,8 +2,10 @@ import styles from './CityList.module.css';
 import CityItem from "./CityItem.jsx";
 import Spinner from "./Spinner.jsx";
 import Message from "./Message.jsx";
+import {useCities} from "../context/CitiesContext.js";
 
-function CityList({cities, isLoading}) {
+function CityList() {
+    const {isLoading, cities} = useCities();
     if(isLoading) return <Spinner/>
 
     if(!cities.length) return <Message message='Add your first city by clicking on a city on the map'/>
