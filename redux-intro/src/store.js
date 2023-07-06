027f3635dcebabe3e84f202ba9c98d0c243ae50d
eskim <eskim@acrofuture.com>
2023-07-06 15:16:44 +0900

    [redux-intro] reducer combine

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,4 +1,4 @@
-import {createStore} from 'redux';
+import {combineReducers, createStore} from 'redux';
 
 const initialState = {
     balance: 0,
@@ -6,7 +6,14 @@ const initialState = {
     loanPurpose: ''
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer ={
+    fullName:'',
+    nationalID:'',
+    createdAt:'',
+    status:''
+}
+
+function accountReducer(state = initialState, action) {
     switch (action.type) {
         case 'account/deposit':
             return {...state, balance: state.balance + action.payload};
@@ -22,10 +29,25 @@ function reducer(state = initialState, action) {
     }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+    switch (action.type) {
+        case 'customer/createCustomer':
+            return {...state, fullName: action.payload.fullName, nationalID: action.payload.nationalID, createdAt: action.payload.createdAt};
+        case 'customer/updateName':
+            return {...state, fullName: action.payload.fullName};
+        default:
+            return state;
+    }
+}
 
-// store.dispatch({type: 'account/deposit', payload: 500});
-// console.log(store.getState());
+// combine reducer
+const rootReducer = combineReducers({
+    account:accountReducer,
+    customer:customerReducer
+});
+
+const store = createStore(rootReducer);
+
 
 function deposit(amount){
     return {type:'account/deposit',payload:amount};
@@ -40,5 +62,13 @@ function payLoan(){
     return {type:'account/payLoan'};
 }
 
-store.dispatch(deposit(500));
-console.log(store.getState());
+function createCustomer(fullName,nationalID){
+    return {type:'customer/createCustomer', payload:{fullName, nationalID, createdAt: new Date().toISOString()}}; // reducer안에서는 side effect없어야 하므로 여기서..
+}
+
+function updateName(fullName) {
+    return {type:'customer/updateName',payload:fullName}
+}
+
+store.dispatch(createCustomer('EUNSUN KIM','23424'));
+console.log(store.getState())
