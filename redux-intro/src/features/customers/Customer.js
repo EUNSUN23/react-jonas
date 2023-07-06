9abba045bcef7b0faf4fad2b75aff6cf6ad81faa
eskim <eskim@acrofuture.com>
2023-07-06 15:33:01 +0900

    [redux-intro] useSelector로 store구독

--- redux-intro/src/features/customers/Customer.js
+++ redux-intro/src/features/customers/Customer.js
@@ -1,5 +1,11 @@
+import {useSelector} from "react-redux";
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+  // useSelector : store를 구독한다. store의 state가 변하면 그 store를 구독하는 컴포넌트가 렌더링된다.
+  const customer = useSelector(store => store.customer.fullName); // reducer 합칠 때 customerReducer에 부여한 이름
+
+  console.log(customer);
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
