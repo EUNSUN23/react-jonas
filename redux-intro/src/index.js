7f84abed254eb634f550bd62b0395147511d5ea3
eskim <eskim@acrofuture.com>
2023-07-06 15:27:03 +0900

    [redux-intro] reducer,store파일분리

--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import './store'; // store.js파일을 실행함
+import store from './store'; // store.js파일을 실행함
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,44 +1,6 @@
 import {combineReducers, createStore} from 'redux';
-
-const initialState = {
-    balance: 0,
-    loan: 0,
-    loanPurpose: ''
-};
-
-const initialStateCustomer ={
-    fullName:'',
-    nationalID:'',
-    createdAt:'',
-    status:''
-}
-
-function accountReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'account/deposit':
-            return {...state, balance: state.balance + action.payload};
-        case 'account/withdraw':
-            return {...state, balance: state.balance - action.payload};
-        case 'account/requestLoan':
-            if (state.loan > 0) return state;
-            return {...state, loan: action.payload.amount, loanPurpose: action.payload.purpose};
-        case 'account/payLoan':
-            return {...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan};
-        default:
-            return state; // context의 reducer와 다르게, 에러 던지는것 권장 x
-    }
-}
-
-function customerReducer(state = initialStateCustomer, action) {
-    switch (action.type) {
-        case 'customer/createCustomer':
-            return {...state, fullName: action.payload.fullName, nationalID: action.payload.nationalID, createdAt: action.payload.createdAt};
-        case 'customer/updateName':
-            return {...state, fullName: action.payload.fullName};
-        default:
-            return state;
-    }
-}
+import {createCustomer, customerReducer} from "./features/customers/customerSlice";
+import {accountReducer} from "./features/accounts/accountSlice";
 
 // combine reducer
 const rootReducer = combineReducers({
@@ -48,27 +10,4 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
-
-function deposit(amount){
-    return {type:'account/deposit',payload:amount};
-}
-function requestLoan(amount,purpose){
-    return {type:'account/requestLoan',payload: {amount:amount, purpose:purpose}};
-}
-function withdraw(payload){
-    return {type:'account/withdraw',payload:payload};
-}
-function payLoan(){
-    return {type:'account/payLoan'};
-}
-
-function createCustomer(fullName,nationalID){
-    return {type:'customer/createCustomer', payload:{fullName, nationalID, createdAt: new Date().toISOString()}}; // reducer안에서는 side effect없어야 하므로 여기서..
-}
-
-function updateName(fullName) {
-    return {type:'customer/updateName',payload:fullName}
-}
-
-store.dispatch(createCustomer('EUNSUN KIM','23424'));
-console.log(store.getState())
+export default store;
