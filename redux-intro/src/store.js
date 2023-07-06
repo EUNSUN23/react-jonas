1dc79d81c2aff919bf35cca5f605f8092b19a389
eskim <eskim@acrofuture.com>
2023-07-06 14:57:11 +0900

    [redux-intro] reducer, store 생성, dispatch

---
+++ redux-intro/src/store.js
@@ -0,0 +1,28 @@
+import {createStore} from 'redux';
+
+const initialState = {
+    balance: 0,
+    loan: 0,
+    loanPurpose: ''
+};
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case 'account/deposit':
+            return {...state, balance: state.balance + action.payload};
+        case 'account/withdraw':
+            return {...state, balance: state.balance - action.payload};
+        case 'account/requestLoan':
+            if (state.loan > 0) return state;
+            return {...state, loan: action.payload.amount, loanPurpose: action.payload.purpose};
+        case 'account/payLoan':
+            return {...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan};
+        default:
+            return state; // context의 reducer와 다르게, 에러 던지는것 권장 x
+    }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({type: 'account/deposit', payload: 500});
+console.log(store.getState());
