8dda0f7e192735241d4e7716c1219308d305597c
eskim <eskim@acrofuture.com>
2023-07-06 17:08:16 +0900

    [redux-intro] redux-devtools 사용

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -2,6 +2,7 @@ import {applyMiddleware, combineReducers, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import {customerReducer} from "./features/customers/customerSlice";
 import {accountReducer} from "./features/accounts/accountSlice";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 // combine reducer
 const rootReducer = combineReducers({
@@ -9,6 +10,9 @@ const rootReducer = combineReducers({
     customer:customerReducer
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
