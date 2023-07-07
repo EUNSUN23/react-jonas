e8204211b4fbe7ddbaeea16a11e7f2cf3b12cec8
eskim <eskim@acrofuture.com>
2023-07-07 13:14:02 +0900

    [wild-oasis] Button,Input분리, eslint에 no-unused-vars:off 추가

--- wild-oasis/src/App.jsx
+++ wild-oasis/src/App.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles.js";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
 
 // 리액트 컴포넌트를 반환한다.
 // jsx props를 받을 수 있다.
@@ -8,23 +10,7 @@ const H1 = styled.h1`
   font-weight: 600;
 `;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
 
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`
 
 // 이미 존재하는 App을 스타일링할때에는 Styled컴포넌트명 이 관례.
 const StyledApp = styled.main`
