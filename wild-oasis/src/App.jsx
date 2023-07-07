966b1d456f94b3f0e68a144f21cc74e136ec0711
eskim <eskim@acrofuture.com>
2023-07-07 13:53:17 +0900

    [wild-oasis] 페이지 & router추가

--- wild-oasis/src/App.jsx
+++ wild-oasis/src/App.jsx
@@ -1,50 +1,32 @@
-import styled from "styled-components";
+import React from 'react';
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import Bookings from "./pages/Bookings.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings.jsx";
+import Account from "./pages/Account.jsx";
+import Login from "./pages/Login.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
-import Button from "./ui/Button.jsx";
-import Input from "./ui/Input.jsx";
-import Heading from "./ui/Heading.jsx";
-import Row from "./ui/Row.jsx";
-
-// 리액트 컴포넌트를 반환한다.
-// jsx props를 받을 수 있다.
-
-
-// 이미 존재하는 App을 스타일링할때에는 Styled컴포넌트명 이 관례.
-const StyledApp = styled.main`
-  padding: 20px;
-`
 
 function App() {
     return (
         <>
-            <GlobalStyles/>
-            <StyledApp>
-                <Row>
-
-                    <Row type='horizontal'>
-                        <Heading as="h1">The wild oasis</Heading>
-                        <div>
-                            <Heading as="h2">Check in and out</Heading>
-                            <Button onClick={() => alert("check in")}>Check in</Button>
-                            <Button
-                                variation='secondary'
-                                size='small'
-                                onClick={() => alert("check out")}
-                            >
-                                Check out
-                            </Button>
-                        </div>
-                    </Row>
-
-                    <Row>
-                        <Heading as="h3">Form</Heading> {/*h3요소로 렌더링됨*/}
-                        <div>
-                            <Input type='number' placeholder='Number of guests'/>
-                            <Input type='number' placeholder='Number of guests'/>
-                        </div>
-                    </Row>
-                </Row>
-            </StyledApp>
+        <GlobalStyles/>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Navigate replace to='dashboard'/>}/>
+                    <Route path='dashboard' element={<Dashboard/>}/>
+                    <Route path='bookings' element={<Bookings/>}/>
+                    <Route path='cabins' element={<Cabins/>}/>
+                    <Route path='users' element={<Users/>}/>
+                    <Route path='settings' element={<Settings/>}/>
+                    <Route path='account' element={<Account/>}/>
+                    <Route path='login' element={<Login/>}/>
+                    <Route path='*' element={<PageNotFound/>}/>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
