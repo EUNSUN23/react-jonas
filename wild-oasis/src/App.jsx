69ef0e2c04b08b2a67ea3807e366dc6692d47364
eskim <eskim@acrofuture.com>
2023-07-07 13:25:01 +0900

    [wild-oasis] 1.styled-component에 props받기 2. as속성으로 시맨틱하게 렌더링하기

--- wild-oasis/src/App.jsx
+++ wild-oasis/src/App.jsx
@@ -2,14 +2,10 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles.js";
 import Button from "./ui/Button.jsx";
 import Input from "./ui/Input.jsx";
+import Heading from "./ui/Heading.jsx";
 
 // 리액트 컴포넌트를 반환한다.
 // jsx props를 받을 수 있다.
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
-
 
 
 // 이미 존재하는 App을 스타일링할때에는 Styled컴포넌트명 이 관례.
@@ -23,9 +19,11 @@ function App() {
         <>
             <GlobalStyles/>
             <StyledApp>
-                <H1>The wild oasis</H1>
+                <Heading as="h1">The wild oasis</Heading>
+                <Heading as="h2">Check in and out</Heading>
                 <Button>Check in</Button>
                 <Button>Check out</Button>
+                <Heading as="h3">Form</Heading> {/*h3요소로 렌더링됨*/}
                 <Input type='number' placeholder='Number of guests'/>
             </StyledApp>
         </>
