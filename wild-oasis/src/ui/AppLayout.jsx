eae0e6c3a8d3230ec8e1bfc3da5c050f88a0ba30
eskim <eskim@acrofuture.com>
2023-07-07 14:13:14 +0900

    [wild-oasis] styled-component > Header, Sidebar, Main 나누기(padding추가)

--- wild-oasis/src/ui/AppLayout.jsx
+++ wild-oasis/src/ui/AppLayout.jsx
@@ -1,16 +1,21 @@
 import {Outlet} from "react-router-dom";
 import Sidebar from "./Sidebar.jsx";
 import Header from "./Header.jsx";
+import styled from "styled-components";
+
+const Main = styled.main`
+  background-color: green;
+  padding: 4rem 4.8rem 6.4rem;
+`
 
 function AppLayout() {
     return (
         <div>
-            <Sidebar/>
             <Header/>
-            <main> {/* 컨텐츠 영역 container */}
+            <Sidebar/>
+            <Main> {/* 컨텐츠 영역 container */}
                 <Outlet/> {/* layout route의 child route컴포넌트 위치 */}
-            </main>
-
+            </Main>
         </div>
     );
 }
