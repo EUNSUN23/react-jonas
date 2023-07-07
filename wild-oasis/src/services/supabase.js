051d30dc2185de83af3e8d66b045b8f68cd65ecf
eskim <eskim@acrofuture.com>
2023-07-07 16:47:23 +0900

    [wild-oasis] supabase > supabase 연동 추가

---
+++ wild-oasis/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://gajgkvgubluimiivcggj.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdhamdrdmd1Ymx1aW1paXZjZ2dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg3MTM0NTAsImV4cCI6MjAwNDI4OTQ1MH0.G7qF5AKxRx7T_HzDdKRzy0vdcUf2eK5FUszr9pTwRgA'
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
