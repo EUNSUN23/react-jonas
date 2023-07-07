63cbbc09f938501d3b4979f5527c946195de260a
eskim <eskim@acrofuture.com>
2023-07-07 14:24:05 +0900

    [wild-oasis] styled-component > AppLayout의 Header, Sidebar, Main 위치 grid로 잡기

--- wild-oasis/src/ui/AppLayout.jsx
+++ wild-oasis/src/ui/AppLayout.jsx
@@ -3,6 +3,15 @@ import Sidebar from "./Sidebar.jsx";
 import Header from "./Header.jsx";
 import styled from "styled-components";
 
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr; /*첫번째 칼럼은 26rem, 나머지 칼럼은 나머지 영역 차지*/
+  grid-template-rows: auto 1fr; /*첫번째 row는 컨텐츠 사이즈, 나머지 row는 나머지 영역 차지*/
+  height: 100vh;
+
+  grid-row: 1 / -1;
+`
+
 const Main = styled.main`
   background-color: green;
   padding: 4rem 4.8rem 6.4rem;
@@ -10,13 +19,13 @@ const Main = styled.main`
 
 function AppLayout() {
     return (
-        <div>
+        <StyledAppLayout>
             <Header/>
             <Sidebar/>
             <Main> {/* 컨텐츠 영역 container */}
                 <Outlet/> {/* layout route의 child route컴포넌트 위치 */}
             </Main>
-        </div>
+        </StyledAppLayout>
     );
 }
 
