0af47fd3728c1c9d631b32e041526ec66d096b4f
eskim <eskim@acrofuture.com>
2023-07-07 14:06:52 +0900

    [wild-oasis] 1. AppLayout > Header, main추가 2. 컨텐츠 영역 컨테이너 태그 추가

--- wild-oasis/src/ui/AppLayout.jsx
+++ wild-oasis/src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
 import {Outlet} from "react-router-dom";
+import Sidebar from "./Sidebar.jsx";
+import Header from "./Header.jsx";
 
 function AppLayout() {
     return (
         <div>
-            <p>AppLayout</p>
-            <Outlet/> {/* layout route의 child route컴포넌트 위치 */}
+            <Sidebar/>
+            <Header/>
+            <main> {/* 컨텐츠 영역 container */}
+                <Outlet/> {/* layout route의 child route컴포넌트 위치 */}
+            </main>
+
         </div>
     );
 }
