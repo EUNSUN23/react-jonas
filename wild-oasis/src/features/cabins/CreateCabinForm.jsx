0200c327d8056816f42ac10ae173c6975f62b05e
eskim <eskim@acrofuture.com>
2023-07-10 11:18:12 +0900

    [wild-oasis] CreateCabinForm > react-hook-form사용 > validation - min 추가

--- wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -52,7 +52,7 @@ function CreateCabinForm() {
 
     const queryClient = useQueryClient();
 
-    const {mutate, isLoading:isCreating} = useMutation({
+    const {mutate, isLoading: isCreating} = useMutation({
         mutationFn: createCabin,
         onSuccess: () => {
             toast.success('New cabin successfully created');
@@ -81,28 +81,49 @@ function CreateCabinForm() {
         <Form onSubmit={handleSubmit(onSubmit, onError)}>
             <FormRow>
                 <Label htmlFor="name">Cabin name</Label>
-                <Input type="text" id="name" {...register('name',
-                    {required:'This field is required'})}/>
+                <Input type="text" id="name"
+                       {...register('name', {
+                           required: 'This field is required'
+                       })}/>
             </FormRow>
 
             <FormRow>
                 <Label htmlFor="maxCapacity">Maximum capacity</Label>
-                <Input type="number" id="maxCapacity" {...register('maxCapacity',{required:'This field is required'})}/>
+                <Input type="number" id="maxCapacity"
+                       {...register('maxCapacity', {
+                           required: 'This field is required',
+                           min: {
+                               value: 1,
+                               message: 'Capacity should be at least 1'
+                           }
+                       })}/>
             </FormRow>
 
             <FormRow>
                 <Label htmlFor="regularPrice">Regular price</Label>
-                <Input type="number" id="regularPrice" {...register('regularPrice',{required:'This field is required'})} />
+                <Input type="number" id="regularPrice"
+                       {...register('regularPrice', {
+                           required: 'This field is required',
+                           min: {
+                               value: 1,
+                               message: 'Capacity should be at least 1'
+                           }
+                       })} />
             </FormRow>
 
             <FormRow>
                 <Label htmlFor="discount">Discount</Label>
-                <Input type="number" id="discount" defaultValue={0} {...register('discount',{required:'This field is required'})}/>
+                <Input type="number" id="discount"
+                       defaultValue={0}
+                       {...register('discount', {
+                           required: 'This field is required',
+                       })}/>
             </FormRow>
 
             <FormRow>
                 <Label htmlFor="description">Description for website</Label>
-                <Textarea type="number" id="description" defaultValue="" {...register('description',{required:'This field is required'})}/>
+                <Textarea type="number" id="description"
+                          defaultValue="" {...register('description', {required: 'This field is required'})}/>
             </FormRow>
 
             <FormRow>
