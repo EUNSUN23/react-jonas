d7bc566c5735f649b18ee21d6f0a4dc479885400
eskim <eskim@acrofuture.com>
2023-07-10 10:28:37 +0900

    [wild-oasis] AppLayout의 Outlet영역 css 수정

--- wild-oasis/src/ui/AppLayout.jsx
+++ wild-oasis/src/ui/AppLayout.jsx
@@ -15,13 +15,23 @@ const Main = styled.main`
   padding: 4rem 4.8rem 6.4rem;
 `
 
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`
+
 function AppLayout() {
     return (
         <StyledAppLayout>
             <Header/>
             <Sidebar/>
             <Main> {/* 컨텐츠 영역 container */}
-                <Outlet/> {/* layout route의 child route컴포넌트 위치 */}
+                <Container>
+                    <Outlet/> {/* layout route의 child route컴포넌트 위치 */}
+                </Container>
             </Main>
         </StyledAppLayout>
     );
