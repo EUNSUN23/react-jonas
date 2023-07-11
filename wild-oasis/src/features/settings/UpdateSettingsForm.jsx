9274236c7ff32d0a3a7610e1d77e6df3cfc026b5
eskim <eskim@acrofuture.com>
2023-07-11 11:00:32 +0900

    [wild-oasis] Settings페이지 state관리 추가

--- wild-oasis/src/features/settings/UpdateSettingsForm.jsx
+++ wild-oasis/src/features/settings/UpdateSettingsForm.jsx
@@ -1,74 +1,77 @@
-import Spinner from 'ui/Spinner';
-import { useSettings } from 'features/settings/useSettings';
-import { useUpdateSetting } from 'features/settings/useUpdateSetting';
+import Spinner from '../../ui/Spinner';
+import useSettings from '../../features/settings/useSettings';
+import useUpdateSetting from '../../features/settings/useUpdateSetting';
 
-import Form from 'ui/Form';
-import FormRow from 'ui/FormRow';
-import Input from 'ui/Input';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
 
 function UpdateSettingsForm() {
-  const {
-    settings: {
-      minBookingLength,
-      maxBookingLength,
-      maxGuestsPerBooking,
-      breakfastPrice,
-    } = {},
-    isLoading,
-  } = useSettings();
-  const { mutate: updateSetting, isLoading: isUpdating } = useUpdateSetting();
+    const {
+        isLoading, settings: {
+            minBookingLength,
+            maxBookingLength,
+            maxGuestsPerBooking,
+            breakfastPrice
+        } = {}
+    } = useSettings();
 
-  // return <Spinner />;
-  if (isLoading) return <Spinner />;
+    console.log("UpdateSettingsForm render, maxGuestsPerBooking: ",maxGuestsPerBooking);
 
-  function handleBlur(e, field) {
-    const { value } = e.target;
+    const {updateSetting, isUpdating} = useUpdateSetting();
 
-    if (!value) return;
-    updateSetting({ [field]: value });
-  }
+    console.log("isLoading: ",isLoading, " isUpdating: ",isUpdating);
 
-  // This time we are using UNCONTROLLED fields, so we will NOT store state
-  return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input
-          type='number'
-          defaultValue={minBookingLength}
-          onBlur={(e) => handleBlur(e, 'minBookingLength')}
-          disabled={isUpdating}
-          id='min-nights'
-        />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input
-          type='number'
-          defaultValue={maxBookingLength}
-          onBlur={(e) => handleBlur(e, 'maxBookingLength')}
-          disabled={isUpdating}
-          id='max-nights'
-        />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input
-          type='number'
-          defaultValue={maxGuestsPerBooking}
-          onBlur={(e) => handleBlur(e, 'maxGuestsPerBooking')}
-          disabled={isUpdating}
-          id='max-guests'
-        />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input
-          type='number'
-          defaultValue={breakfastPrice}
-          onBlur={(e) => handleBlur(e, 'breakfastPrice')}
-          disabled={isUpdating}
-          id='breakfast-price'
-        />
-      </FormRow>
-    </Form>
-  );
+    if (isLoading) return <Spinner/>;
+
+    function handleBlur(e, field) {
+        const {value} = e.target;
+
+        if (!value) return;
+        updateSetting({[field]: value});
+    }
+
+    // This time we are using UNCONTROLLED fields, so we will NOT store state
+    return (
+        <Form>
+            <FormRow label='Minimum nights/booking'>
+                <Input
+                    type='number'
+                    defaultValue={minBookingLength}
+                    onBlur={(e) => handleBlur(e, 'minBookingLength')} // 포커스가 input에서 벗어나면 handleBlur실행
+                    disabled={isUpdating}
+                    id='min-nights'
+                />
+            </FormRow>
+            <FormRow label='Maximum nights/booking'>
+                <Input
+                    type='number'
+                    defaultValue={maxBookingLength}
+                    onBlur={(e) => handleBlur(e, 'maxBookingLength')}
+                    disabled={isUpdating}
+                    id='max-nights'
+                />
+            </FormRow>
+            <FormRow label='Maximum guests/booking'>
+                <Input
+                    type='number'
+                    defaultValue={maxGuestsPerBooking}
+                    onBlur={(e) => handleBlur(e, 'maxGuestsPerBooking')}
+                    disabled={isUpdating}
+                    id='max-guests'
+                />
+            </FormRow>
+            <FormRow label='Breakfast price'>
+                <Input
+                    type='number'
+                    defaultValue={breakfastPrice}
+                    onBlur={(e) => handleBlur(e, 'breakfastPrice')}
+                    disabled={isUpdating}
+                    id='breakfast-price'
+                />
+            </FormRow>
+        </Form>
+    );
 }
 
 export default UpdateSettingsForm;
---
+++ wild-oasis/src/features/settings/useUpdateSetting.js
@@ -0,0 +1,23 @@
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {updateSetting as updateSettingApi} from "../../services/apiSettings.js";
+import toast from "react-hot-toast";
+
+function useUpdateSetting() {
+    const queryClient = useQueryClient();
+    const {isLoading:isUpdating, mutate:updateSetting} = useMutation({
+        mutationFn:updateSettingApi,
+        onSuccess:()=>{
+            toast.success("Setting successfully edited");
+            queryClient.invalidateQueries({
+                queryKey:['settings']
+            })
+        },
+        onError:(err)=> toast.error(err.message)
+    });
+
+    return {updateSetting,isUpdating};9854
+
+
+}
+
+export default useUpdateSetting;
